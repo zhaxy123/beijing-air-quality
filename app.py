88c85e1f849e0ead459906fe559236d82ed7f943
zhaxy123 <913925846@qq.com>
2025-11-27 10:39:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -351,7 +351,7 @@ def main():
         st.session_state.animation_data = None
     
     if 'app' not in st.session_state:
-        st.session_state.app = BeijingAirQualityStreamlitApp('./aqi_hourly.csv')
+        st.session_state.app = BeijingAirQualityStreamlitApp('project_data/aqi_hourly.csv')
     
     app = st.session_state.app
     
@@ -551,4 +551,5 @@ def main():
 if __name__ == "__main__":
 
     main()
+
 
