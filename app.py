043d61ac133e6c368ab584611f0b8d41cc4f2dab
zhaxy123 <913925846@qq.com>
2025-11-27 10:42:52 +0800

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -51,7 +51,6 @@ class BeijingAirQualityStreamlitApp:
             encodings = ['gbk', 'gb2312', 'utf-8', 'latin1', 'iso-8859-1']
             for encoding in encodings:
                 try:
-                    print(self.hourly_data_path)
                     self.df_hourly = pd.read_csv(self.hourly_data_path, encoding=encoding)
                     self.preprocess_data()
                     break
@@ -352,7 +351,7 @@ def main():
         st.session_state.animation_data = None
     
     if 'app' not in st.session_state:
-        st.session_state.app = BeijingAirQualityStreamlitApp('project_data/aqi_hourly.csv')
+        st.session_state.app = BeijingAirQualityStreamlitApp('aqi_hourly.csv')
     
     app = st.session_state.app
     
@@ -552,6 +551,7 @@ def main():
 if __name__ == "__main__":
 
     main()
+
 
 
 
